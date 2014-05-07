ff228594a224601aef31542d025b304fce958564
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-05-07 19:07:41 +0200

    build

--- test-classes-moyennes-app/js/main.js
+++ test-classes-moyennes-app/js/main.js
@@ -2,7 +2,7 @@
 /* global angular */
 
 require.config({
-	urlArgs: "bust=" + Math.random(), //'1399018451225',
+	urlArgs: "bust=" + Math.random(), //'1399482445480',
 	paths: {
 		angular: '/test-classes-moyennes-app/vendor/angular/angular',
 		angularRoute: '/test-classes-moyennes-app/vendor/angular-route/angular-route',
